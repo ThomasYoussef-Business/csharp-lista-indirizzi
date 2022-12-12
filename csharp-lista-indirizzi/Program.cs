04e267973356ce0c99727c5849f4b4c35576756c
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-12 17:12:06 +0100

    Ho fatto tutto il refactoring che potevo, non penso di sapere come migliorare la situazione

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -9,19 +9,17 @@ string path = "C:\\Users\\thoma\\Desktop\\Desktop\\Programming\\NET Course\\repo
 string contenuto = "";
 string[] linee;
 List<Indirizzo> indirizzi = new();
-StreamReader fileIndirizzi;
 
-if (!File.Exists(path)) {
-    throw new FileNotFoundException($"Il file {path} specificato non esiste!", path);
+using (StreamReader fileIndirizzi = File.OpenText(path)) {
+    contenuto = fileIndirizzi.ReadToEnd();
 }
 
-fileIndirizzi = File.OpenText(path);
-contenuto = fileIndirizzi.ReadToEnd();
 linee = contenuto.Split('\n');
-fileIndirizzi.Close();
 
 for (int i = 1; i < linee.Length; i++) {
-    string[] datiIndirizzo = linee[i].Split(',');
+    string linea = linee[i];
+    string[] datiIndirizzo = LineaProcessata(linea);
+
     if (DatiValidi(datiIndirizzo)) {
         Indirizzo nuovoIndirizzo = new(datiIndirizzo);
         indirizzi.Add(nuovoIndirizzo);
@@ -32,7 +30,16 @@ foreach (Indirizzo indirizzo in indirizzi) {
     Console.WriteLine($"{indirizzo}\n");
 }
 
+
+// -------------------------------------------------------------------
+
 static bool DatiValidi(string[] datiIndirizzo) {
     return datiIndirizzo.Length == 6 &&
         !datiIndirizzo.Any(s => string.IsNullOrWhiteSpace(s));
+}
+
+static string[] LineaProcessata(string linea) {
+    return linea.Split(',')
+                .Select(s => s.Trim())
+                .ToArray();
 }
