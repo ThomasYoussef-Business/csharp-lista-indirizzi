3233a35ff574d397f2e9e214091d807fa595aca5
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-12 15:08:51 +0100

    Aggiunta classe Indirizzo

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,31 @@
+﻿namespace csharp_lista_indirizzi {
+    public class Indirizzo {
+        // ATTRIBUTI
+
+
+        // PROPRIETÀ
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Strada { get; set; }
+        public string Citta { get; set; }
+        public string Provincia { get; set; }
+        public string ZIP { get; set; }
+
+        // COSTRUTTORI
+        public Indirizzo(string nome, string cognome, string strada, string citta, string provincia, string zip) {
+            Nome = nome;
+            Cognome = cognome;
+            Strada = strada;
+            Citta = citta;
+            Provincia = provincia;
+            ZIP = zip;
+        }
+
+
+        // METODI PUBBLICI
+
+
+        // METODI PRIVATI
+
+    }
+}
