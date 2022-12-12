59dd37a54f2c3ce0e253d94ed4056b64dfe8401b
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-12 16:40:05 +0100

    Creata una bozza per il mio metodo di spacchettare i dati dal file. Refactoring in corso per renderlo più leggibile!

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Il compito è di leggere un file CSV, e salvare tutti gli indirizzi contenuti al suo interno
+ * in una lista di oggetti istanziati a partire dalla classe Indirizzo.
+ */
+
+using csharp_lista_indirizzi;
+
+string path = "C:\\Users\\thoma\\Desktop\\Desktop\\Programming\\NET Course\\repos\\csv-files\\addresses.csv";
+string contenuto = "";
+string[] linee;
+List<Indirizzo> indirizzi = new();
+StreamReader fileIndirizzi;
+
+if (!File.Exists(path)) {
+    throw new FileNotFoundException($"Il file {path} specificato non esiste!", path);
+}
+
+fileIndirizzi = File.OpenText(path);
+contenuto = fileIndirizzi.ReadToEnd();
+linee = contenuto.Split('\n');
+fileIndirizzi.Close();
+
+for (int i = 1; i < linee.Length; i++) {
+    string[] datiIndirizzo = linee[i].Split(',');
+    if (DatiValidi(datiIndirizzo)) {
+        Indirizzo nuovoIndirizzo = new(datiIndirizzo);
+        indirizzi.Add(nuovoIndirizzo);
+    }
+}
+
+foreach (Indirizzo indirizzo in indirizzi) {
+    Console.WriteLine($"{indirizzo}\n");
+}
+
+static bool DatiValidi(string[] datiIndirizzo) {
+    return datiIndirizzo.Length == 6 &&
+        !datiIndirizzo.Any(s => string.IsNullOrWhiteSpace(s));
+}
